4e1910925a263446fa75c45c801fdebce12b1ff4
Yangjin Cho <7033942cyj@dongguk.edu>
2021-07-28 21:20:56 +0900

    Add files via upload

---
+++ ObservableCreateExample.java
@@ -0,0 +1,17 @@
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+
+public class ObservableCreateExample {
+    public static void main(String[] args){
+        // cold type Observable
+        Observable<Integer> source = Observable.create(
+                (ObservableEmitter<Integer> emitter) -> {
+                    emitter.onNext(100);
+                    emitter.onNext(200);
+                    emitter.onNext(300);
+                    emitter.onComplete();
+                }
+        );
+        source.subscribe(System.out::println);
+    }
+}
